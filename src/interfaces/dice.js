518ea7e85711539b0ba0286e73da3773212f08eb
Vinay Kiran Polisetti <vinaykiranbalasairamsurya.polisetti@coinbase.com>
2024-07-09 22:27:26 +0530

    Added Dice interface

---
+++ src/interfaces/dice.js
@@ -0,0 +1,8 @@
+// Dice interface that requires the implementation of the roll method.
+class Dice {
+    roll() {
+        throw new Error("Method 'roll()' must be implemented.");
+    }
+}
+
+module.exports = Dice;
